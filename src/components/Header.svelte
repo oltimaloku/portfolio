1969c79d473a08bf1b38513b2a9f9c3195624207
oltimaloku <oltimaloku555@gmail.com>
2024-01-03 01:09:20 -0800

    Fixed some header links

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,29 +1,47 @@
 <script>
-    export let y;
-    let tabs = [
-        {name: 'Projects', link: '#projects'},
-        {name: 'About me', link: '#about'},
-        //{name: 'Blog', '#projects'},
-    ]
+  import { onMount } from "svelte";
+
+  export let y;
+  let baseUrl;
+  let tabs = [];
+
+  onMount(() => {
+    baseUrl = window.location.origin; // Set base URL after component mounts
+    tabs = [
+      { name: "Projects", link: `${baseUrl}/#projects` },
+      { name: "About me", link: `${baseUrl}/#about` },
+      // {name: 'Blog', link: `${baseUrl}/#blog`}, // Uncomment and edit as needed
+    ];
+  });
 </script>
 
-<header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid " + (
-    y > 0 ? ' py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent'
-)}>
+<header
+  class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid " +
+    (y > 0
+      ? " py-4 bg-slate-950 border-violet-950"
+      : "py-6 bg-transparent border-transparent")}
+>
+  <a href={baseUrl}>
     <h1 class="font-medium">
-        <b class="font-bold poppins">Olti</b> Maloku
+      <b class="font-bold poppins">Olti</b> Maloku
     </h1>
-    <div class="sm:flex items-center gap-4 hidden">
-        {#each tabs as tab, index}
-            <a href={tab.link} class="duration-200 hover:text-violet-400">
-                <p>{tab.name}</p>
-            </a>
-        {/each}
-    </div>
-    <a href="https://www.linkedin.com/in/oltimaloku/" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
-        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+  </a>
 
-        </div>
-        <h4 class="relative z-9">Get in touch</h4>
-    </a>
+  <div class="sm:flex items-center gap-4 hidden">
+    {#each tabs as tab, index}
+      <a href={tab.link} class="duration-200 hover:text-violet-400">
+        <p>{tab.name}</p>
+      </a>
+    {/each}
+  </div>
+  <a
+    href="https://www.linkedin.com/in/oltimaloku/"
+    target="_blank"
+    class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+  >
+    <div
+      class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+    ></div>
+    <h4 class="relative z-9">Get in touch</h4>
+  </a>
 </header>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -105,13 +105,7 @@
         Curious to <span class="poppins text-violet-400">see</span> my work?
       </h3>
     </div>
-    <a
-      href=""
-      target="_blank"
-      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
-    >
-      <i class="fa-regular fa-circle-play" />
-    </a>
+
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
       <Step step={steps[0]}>
         <p class="py-8">
