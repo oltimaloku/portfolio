3730853fc1ca86d7b54b326ca0f416a5c21d08ca
oltimaloku <oltimaloku555@gmail.com>
2024-01-03 18:51:51 -0800

    Removed unused code

--- src/routes/projects/[project]/+page.js
+++ src/routes/projects/[project]/+page.js
@@ -1,24 +1,3 @@
-import Idea from "/Users/oltimaloku/Documents/Projects/olti_portfolio/src/components/projects/Idea.svelte";
-/*
-export async function load({ params }) {
-	try {
-		
-		const post = await import(`../${params.project}.md`);
-		
-		const { title, date } = post.metadata;
-		const content = post.default;
-		console.log(content);
-		return {
-			content,
-			title,
-			date
-		};
-	} catch (e) {		
-		console.error(e);
-	}
-	
-}
-*/
 export async function load({ params }) {
 	return {
 		project: params.project,
