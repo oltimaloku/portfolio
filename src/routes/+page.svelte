b18918ba3eb70d572a8caa8cf1706d4f94c7c619
oltimaloku <oltimaloku555@gmail.com>
2024-01-04 15:17:16 -0800

    Removed particle effects from project pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,34 @@
 <script>
-    import Main from '../components/Main.svelte';
+  import Main from "../components/Main.svelte";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js";
+    script.integrity =
+      "sha512-Kef5sc7gfTacR7TZKelcrRs15ipf7+t+n7Zh6mKNJbmW+/RRdCW9nwfLn4YX0s2nO6Kv5Y2ChqgIakaC6PW09A==";
+    script.crossOrigin = "anonymous";
+    script.onload = () => {
+      window.particlesJS.load("particles-js", "/assets/particles.json", () => {
+        console.log("particles.js config loaded");
+      });
+    };
+    document.head.appendChild(script);
+  });
 </script>
 
-<Main/>
+<Main />
+
+<div id="particles-js" class="particles-container"></div>
+
+<style>
+  .particles-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1; /* Ensure it's behind other content */
+  }
+</style>
