d1d8567a89bc2e2bdc109d96d7b3f080222d7ced
oltimaloku <oltimaloku555@gmail.com>
2024-01-03 18:57:28 -0800

    added new config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 import { mdsvex } from 'mdsvex';
-const dev = process.argv.includes('dev');
+const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : process.env.BASE_PATH,
+      base: isProduction ? process.env.BASE_PATH : '',
     }
   },
   extensions: ['.svelte', '.md', '.svx'],
